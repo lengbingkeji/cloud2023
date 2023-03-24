f424b530ced02b96b9912ef994dc842dd4077a8c
heweizhuang <1058104620@qq.com>
2023-03-24 17:36:19 +0800

    SpringCloud Stream 消息驱动

---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/lengbingkeji/springcloud/controller/SendMessageController.java
@@ -0,0 +1,27 @@
+package com.lengbingkeji.springcloud.controller;
+
+import com.lengbingkeji.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * ClassName: SendMessageController
+ * Package: com.lengbingkeji.springcloud.controller
+ * Description:
+ *
+ * @Author lengbing
+ * @Create 2023/3/24 16:50
+ * @Version 1.0
+ */
+@RestController
+public class SendMessageController {
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage(){
+        return messageProvider.send();
+    }
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/lengbingkeji/springcloud/service/IMessageProvider.java
@@ -0,0 +1,14 @@
+package com.lengbingkeji.springcloud.service;
+
+/**
+ * ClassName: IMessageProvider
+ * Package: com.lengbingkeji.springcloud.service
+ * Description:
+ *
+ * @Author lengbing
+ * @Create 2023/3/24 16:42
+ * @Version 1.0
+ */
+public interface IMessageProvider {
+    public String send();
+}
