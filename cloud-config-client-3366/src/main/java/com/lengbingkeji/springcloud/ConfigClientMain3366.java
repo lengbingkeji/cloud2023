37f7b3eee8d02a73e144398a2a7255ddf0577f63
heweizhuang <1058104620@qq.com>
2023-03-24 16:19:55 +0800

    SpringCloud Bus 消息总线

---
+++ cloud-config-client-3366/src/main/java/com/lengbingkeji/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,22 @@
+package com.lengbingkeji.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * ClassName: ConfigClientMain3366
+ * Package: com.lengbingkeji.springcloud
+ * Description:
+ *
+ * @Author lengbing
+ * @Create 2023/3/24 15:54
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
